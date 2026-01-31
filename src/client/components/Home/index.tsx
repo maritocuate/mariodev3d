e5c2569b8f4be2b037cd0077a66661de4bb72f67
marioq <mario300@gmail.com>
2026-01-31 17:42:32 -0300

    chore: Update home animation

--- src/client/components/Home/index.tsx
+++ src/client/components/Home/index.tsx
@@ -27,8 +27,8 @@ function Home() {
             opacity: 0,
             stagger: 0.03,
             ease: "power3.out",
-            duration: 0.6,
-            delay: 0.9,
+            duration: 0.8,
+            delay: 1,
         })
         gsap.from(subtitleRef.current, {
             opacity: 0,
@@ -48,17 +48,17 @@ function Home() {
 
         return () => {
             split.revert()
-          }
+        }
     }, [])
 
-    return(
+    return (
         <div ref={sectionRef} className="section">
             <section className="title-section dimension">
                 <h1 ref={titleRef} className="header">Mario Quiroga</h1>
                 <p ref={subtitleRef} className="subtitle">software developer</p>
             </section>
         </div>
-    )   
+    )
 }
 
 export default Home

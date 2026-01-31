ae6ef5a03fcc97cdedd67c26e4312c40a081dff7
maritocuate <mario300@gmail.com>
2026-01-31 19:13:13 -0300

    fix: Update os styles

--- src/client/components/Home/index.tsx
+++ src/client/components/Home/index.tsx
@@ -1,71 +1,76 @@
-import { useRef } from 'react'
-import gsap from "gsap"
-import { useGSAP } from '@gsap/react'
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { SplitText } from "gsap/SplitText"
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 
-import './Home.css'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import "./Home.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function Home() {
-    const sectionRef = useRef<HTMLDivElement>(null)
-    const titleRef = useRef<HTMLDivElement>(null)
-    const subtitleRef = useRef<HTMLParagraphElement>(null)
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
+  const subtitleRef = useRef<HTMLParagraphElement>(null);
 
-    useGSAP(() => {
-        if (!sectionRef.current || !titleRef.current || !subtitleRef.current) return
+  useGSAP(() => {
+    if (!sectionRef.current || !titleRef.current || !subtitleRef.current)
+      return;
 
-        const split = new SplitText(titleRef.current, {
-            type: "chars,words,lines",
-            mask: "lines",
-            linesClass: "line"
-        })
+    const split = new SplitText(titleRef.current, {
+      type: "chars,words,lines",
+      mask: "lines",
+      linesClass: "line",
+    });
 
-        gsap.from(split.lines, {
-            yPercent: 100,
-            opacity: 0,
-            stagger: 0.03,
-            ease: "power3.out",
-            duration: 0.8,
-            delay: 1,
-        })
-        gsap.from(subtitleRef.current, {
-            opacity: 0,
-            delay: 1.2
-        })
+    gsap.from(split.lines, {
+      yPercent: 100,
+      opacity: 0,
+      stagger: 0.03,
+      ease: "power3.out",
+      duration: 0.8,
+      delay: 1,
+    });
+    gsap.from(subtitleRef.current, {
+      opacity: 0,
+      delay: 1.5,
+    });
 
-        gsap.to(sectionRef.current, {
-            opacity: 0,
-            scale: 1.1,
-            scrollTrigger: {
-                trigger: "#scroll-area",
-                start: "top top",
-                end: "top+=400 top",
-                scrub: true
-            }
-        })
+    gsap.to(sectionRef.current, {
+      opacity: 0,
+      scale: 1.1,
+      scrollTrigger: {
+        trigger: "#scroll-area",
+        start: "top top",
+        end: "top+=400 top",
+        scrub: true,
+      },
+    });
 
-        return () => {
-            split.revert()
-        }
-    }, [])
+    return () => {
+      split.revert();
+    };
+  }, []);
 
-    return (
-        <div ref={sectionRef} className="section">
-            <section className="title-section dimension">
-                <h1 ref={titleRef} className="header">Mario Quiroga</h1>
-                <p ref={subtitleRef} className="subtitle">software developer</p>
-            </section>
-            <DotLottieReact
-                className='scroll-down-animation'
-                src="assets/scrolldown.json"
-                loop
-                autoplay
-            />
-        </div>
-    )
+  return (
+    <div ref={sectionRef} className="section">
+      <section className="title-section dimension">
+        <h1 ref={titleRef} className="header">
+          Mario Quiroga
+        </h1>
+        <p ref={subtitleRef} className="subtitle">
+          software developer
+        </p>
+      </section>
+      <DotLottieReact
+        className="scroll-down-animation"
+        src="assets/scrolldown.json"
+        loop
+        autoplay
+      />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
